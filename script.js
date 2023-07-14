6d922eb6785283324ec20d73de5d976d09ecac2b
shawky_53 <mostafashawky100000@gmail.com>
2023-07-14 05:29:17 +0300

    get total price

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ let total = document.getElementById("total");
 let count = document.getElementById("count");
 let category = document.getElementById("category");
 let submit = document.getElementById("submit");
+
+//get total price
+function getTotal() {
+  if (price.value != "") {
+    let result = +price.value + +taxes.value + +ads.value - +discount.value;
+    total.innerHTML = result;
+    total.style.backgroundColor = "green";
+  } else {
+    total.innerHTML = "";
+    total.style.backgroundColor = "red";
+  }
+}
