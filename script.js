95715892d26af6c9a90e55ade3083ddf3e5d1510
shawky_53 <mostafashawky100000@gmail.com>
2023-07-22 05:31:03 +0300

    clean data

--- script.js
+++ script.js
@@ -42,6 +42,22 @@ function createProduct() {
     count: count.value,
     category: category.value,
   };
+
+  // Check if all required fields are filled
+  if (
+    title.value === "" ||
+    price.value === "" ||
+    taxes.value === "" ||
+    ads.value === "" ||
+    discount.value === "" ||
+    total.innerHTML === "" ||
+    count.value === "" ||
+    category.value === ""
+  ) {
+    alert("Please fill all fields");
+    return; // Exit the function if not all fields are filled
+  }
+
   // Create product by count
   if (mood === "create") {
     if (product.count > 1) {
@@ -55,21 +71,21 @@ function createProduct() {
     dataProduct[temp] = product;
     mood = "create";
     submit.innerHTML = `<button type="button" class="button" id="submit">
-                        <span class="button__text">Create</span>
-                        <span class="button__icon"
-                          ><svg
-                            class="svg"
-                            height="48"
-                            viewBox="0 0 48 48"
-                            width="48"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M35.3 12.7c-2.89-2.9-6.88-4.7-11.3-4.7-8.84 0-15.98 7.16-15.98 16s7.14 16 15.98 16c7.45 0 13.69-5.1 15.46-12h-4.16c-1.65 4.66-6.07 8-11.3 8-6.63 0-12-5.37-12-12s5.37-12 12-12c3.31 0 6.28 1.38 8.45 3.55l-6.45 6.45h14v-14l-4.7 4.7z"
-                            ></path>
-                            <path d="M0 0h48v48h-48z" fill="none"></path></svg
-                        ></span>
-                      </button>`;
+                      <span class="button__text">Create</span>
+                      <span class="button__icon"
+                        ><svg
+                          class="svg"
+                          height="48"
+                          viewBox="0 0 48 48"
+                          width="48"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M35.3 12.7c-2.89-2.9-6.88-4.7-11.3-4.7-8.84 0-15.98 7.16-15.98 16s7.14 16 15.98 16c7.45 0 13.69-5.1 15.46-12h-4.16c-1.65 4.66-6.07 8-11.3 8-6.63 0-12-5.37-12-12s5.37-12 12-12c3.31 0 6.28 1.38 8.45 3.55l-6.45 6.45h14v-14l-4.7 4.7z"
+                          ></path>
+                          <path d="M0 0h48v48h-48z" fill="none"></path></svg
+                      ></span>
+                    </button>`;
     window.scroll({
       top: 0,
       behavior: "smooth",
@@ -84,6 +100,8 @@ function createProduct() {
   // Show data in table
   showData();
 }
+
+// ... (remaining code)
 
 // Function to clear data after submit
 function clearData() {
@@ -358,7 +376,6 @@ function updateProduct(i) {
   temp = i;
 }
 
-// Function to search product
 // Function to search product
 let searchMode = "title";
 function getSearchMode(id) {
